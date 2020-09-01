0f23c81d39e4e45bf8dd7e25d553655bcaa838b9
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-01 13:54:44 -0300

    Categoria DTO

--- src/main/java/com/coursespringboot/workshop/services/CategoriaService.java
+++ src/main/java/com/coursespringboot/workshop/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.coursespringboot.workshop.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityJpaException("Erro de integridate. Não pode excluir categorias com associações. ");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 		
 	}
+	
 }

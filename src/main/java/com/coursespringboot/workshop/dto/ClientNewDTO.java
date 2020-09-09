dacaac7ab8bdf8a5bb4a797b6095ae948562c64b
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-09 11:16:51 -0300

    Validacao CPFouCNPJ

--- src/main/java/com/coursespringboot/workshop/dto/ClientNewDTO.java
+++ src/main/java/com/coursespringboot/workshop/dto/ClientNewDTO.java
@@ -2,11 +2,19 @@ package com.coursespringboot.workshop.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.coursespringboot.workshop.services.validation.ClientInsert;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@ClientInsert
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -14,17 +22,31 @@ import lombok.Setter;
 public class ClientNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "Tamanho invalido. Minimo 5 e maximo 120.")
 	private String nome;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
-
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
 	private String complemento;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
---
+++ src/main/java/com/coursespringboot/workshop/services/validation/ClientInsertValidator.java
@@ -0,0 +1,40 @@
+package com.coursespringboot.workshop.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.coursespringboot.workshop.domain.enums.TipoCliente;
+import com.coursespringboot.workshop.dto.ClientNewDTO;
+import com.coursespringboot.workshop.resources.exceptions.FieldMessage;
+import com.coursespringboot.workshop.services.validation.utils.BR;
+
+public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	@Override
+	public void initialize(ClientInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCode()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCode()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}

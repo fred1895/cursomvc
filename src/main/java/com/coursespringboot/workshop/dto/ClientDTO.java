ea9657ca0e59761b60c7a2b86b66bf48a86ad80b
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-09 14:23:18 -0300

    Validando email cliente no update

--- src/main/java/com/coursespringboot/workshop/dto/ClientDTO.java
+++ src/main/java/com/coursespringboot/workshop/dto/ClientDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.coursespringboot.workshop.domain.Client;
+import com.coursespringboot.workshop.services.validation.ClientUpdate;
 
+@ClientUpdate
 public class ClientDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 

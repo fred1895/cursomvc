4a81542ea714ddfc5dab6665b6b0552e3c8a82d6
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-12 18:13:51 -0300

    Adicionando busca de produtos

--- src/main/java/com/coursespringboot/workshop/repositories/ProdutoRepository.java
+++ src/main/java/com/coursespringboot/workshop/repositories/ProdutoRepository.java
@@ -1,11 +1,23 @@
 package com.coursespringboot.workshop.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.coursespringboot.workshop.domain.Categoria;
 import com.coursespringboot.workshop.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	@Transactional
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.name LIKE %:name% AND cat IN :categorias")
+	Page<Produto> search(@Param("name") String name, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 	
 }
--- src/main/java/com/coursespringboot/workshop/services/ProdutoService.java
+++ src/main/java/com/coursespringboot/workshop/services/ProdutoService.java
@@ -1,12 +1,19 @@
 package com.coursespringboot.workshop.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.coursespringboot.workshop.domain.Categoria;
 import com.coursespringboot.workshop.domain.Produto;
+import com.coursespringboot.workshop.repositories.CategoriaRepository;
 import com.coursespringboot.workshop.repositories.ProdutoRepository;
+import com.coursespringboot.workshop.services.exceptions.ObjetoNaoEncontradoException;
 
 @Service
 public class ProdutoService {
@@ -14,8 +21,18 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repo;
 	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public Produto findById(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
-		return obj.orElse(null); 
+		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String name, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy); 
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(name, categorias, pageRequest);
 	}
 }

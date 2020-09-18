cd28a6948cf8b16722393289105ae17f5a482982
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-18 10:02:29 -0300

    Implementando autorizacao

--- src/main/java/com/coursespringboot/workshop/config/SecurityConfig.java
+++ src/main/java/com/coursespringboot/workshop/config/SecurityConfig.java
@@ -23,37 +23,46 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private JWTUtil jwtUtil;
+	private UserDetailsService userDetailsService;
 	
 	@Autowired
-	private Environment env;
-
+    private Environment env;
+	
 	@Autowired
-	private UserDetailsService userDetailsService;
+	private JWTUtil jwtUtil;
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+	};
 
-	public static final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/produtos/**", "/categorias/**" };
-
-	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**", "/clientes/**" };
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**",
+			"/clientes/**"
+	};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		if (Arrays.asList(env.getActiveProfiles()).contains("teste")) {
-			http.headers().frameOptions().disable();
-		}
-
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-		.anyRequest().authenticated();
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-
+	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

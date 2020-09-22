b2b1bd7760926de754aa2beef67debaead3f42f2
Fred Almeida <fredericonascimento97@gmail.com>
2020-09-22 15:56:40 -0300

    Adicionadno Gmail ao prod 3

--- src/main/java/com/coursespringboot/workshop/services/MailService.java
+++ src/main/java/com/coursespringboot/workshop/services/MailService.java
@@ -1,10 +1,12 @@
 package com.coursespringboot.workshop.services;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.coursespringboot.workshop.domain.Client;
 import com.coursespringboot.workshop.domain.Pedido;
 
+@Service
 public interface MailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
